f3e5f7944a1810ffd8dd4e1e46bb3e5288a4fde6
Andrey Torres <atorresf20@gmail.com>
2023-06-25 18:13:38 -0600

    Automated the zshrc commands

---
+++ zshrc_custom_commands.sh
@@ -0,0 +1,10 @@
+# Git dit change accordingly
+echo alias cdg="~/Documents" >> ~/.zshrc 
+# Refresh git dir and pull
+echo alias grefresh="git fetch --prune origin && git pull --ff" >> ~/.zshrc
+# copy nvim settings from git local master to the config/
+echo alias cpnvimconfig="cp -r ~/.config/nvim ~/Documents/dot-files/ && cd ~/Documents/dot-files/" >> ~/.zshrc
+
+
+# Add to ~/.tmux.conf in case we do not see colors
+echo set -g default-terminal "screen-256color" >> ~/.tmux.conf
