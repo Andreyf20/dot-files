6eee9c91a8ae9d679c9a71507631ccc7716a6ffe
Andrey Torres <atorresf20@gmail.com>
2023-07-03 19:42:08 -0600

    Added highlight to yanked text

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,12 @@
 require("andreyf20")
+
+-- Highlight yanked text
+vim.cmd [[
+  augroup highlight_yank
+    autocmd!
+    au TextYankPost * silent! lua vim.highlight.on_yank({ higroup="IncSearch", timeout=100 })
+  augroup END
+]]
+
+-- Could play around with this more to set a custom color but works for now
+-- vim.api.nvim_set_hl(0, 'highlight_yank', { fg = "green", bg = "black" })
