064eeb0c61d84dda33b3a5ed42deb0e7e4bad33c
Andrey Torres <atorresf20@gmail.com>
2023-07-08 22:29:44 -0600

    Fixed zsh_command output

--- zshrc_custom_commands.sh
+++ zshrc_custom_commands.sh
@@ -1,24 +1,24 @@
 # Git dir, change accordingly
-echo alias cdg="cd ~/Documents" >> ~/.zshrc 
+echo 'alias cdg="cd ~/Documents"' >> ~/.zshrc 
 # Refresh git repo and pull
-echo alias grefresh="git fetch --prune origin && git pull --ff" >> ~/.zshrc
+echo 'alias grefresh="git fetch --prune origin && git pull --ff"' >> ~/.zshrc
 # Copy nvim settings from local to the repo dir
-echo alias cpnvimconfig="cp -r ~/.config/nvim ~/Documents/dot-files/ && cd ~/Documents/dot-files/" >> ~/.zshrc
+echo 'alias cpnvimconfig="cp -r ~/.config/nvim ~/Documents/dot-files/ && cd ~/Documents/dot-files/"' >> ~/.zshrc
 # Open neovim
-echo alias n="nvim ./" >> ~/.zshrc
-echo alias nzsh="nvim ~/.zshrc" >> ~/.zshrc
-echo alias nconfig="nvim ~/.config/nvim" >> ~/.zshrc
+echo 'alias n="nvim ./"' >> ~/.zshrc
+echo 'alias nzsh="nvim ~/.zshrc"' >> ~/.zshrc
+echo 'alias nconfig="nvim ~/.config/nvim"' >> ~/.zshrc
 # Setup go path
-echo export GOPATH="$HOME/go" >> ~/.zshrc
-echo export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin >> ~/.zshrc
+echo 'export GOPATH="$HOME/go"' >> ~/.zshrc
+echo 'export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin' >> ~/.zshrc
 # Setup dotnet path
-echo export PATH="$PATH:$HOME/.dotnet/tools" >> ~/.zshrc
+echo 'export PATH="$PATH:$HOME/.dotnet/tools"' >> ~/.zshrc
 
 # Tmux conf
-echo set -g default-terminal "screen-256color" >> ~/.tmux.conf
-echo set -as terminal-features ",xterm-256color:RGB" >> ~/.tmux.conf
-echo set -g base-index 1 >> ~/.tmux.conf
-echo setw -g pane-base-index 1 >> ~/.tmux.conf
+echo 'set -g default-terminal "screen-256color"' >> ~/.tmux.conf
+echo 'set -as terminal-features ",xterm-256color:RGB"' >> ~/.tmux.conf
+echo 'set -g base-index 1' >> ~/.tmux.conf
+echo 'setw -g pane-base-index 1' >> ~/.tmux.conf
 
 # Copy nvim config to HOME
 cp -r ./nvim ~/.config/ &&
