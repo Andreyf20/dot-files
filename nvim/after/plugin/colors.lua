8998a1ea00677866520439c0a85c7436d2ff1615
Andrey Torres <andrey.e.torres.calderon@cr.ey.com>
2023-10-31 15:42:22 -0600

    Updated number line color in nvim

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -16,9 +16,12 @@ function ColorTerminal(color)
 	-- vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
 	-- vim.api.nvim_set_hl(0, "NvimTreeNormalFloat", { bg = "none" })
 
-	-- Set number line colors, CursorLineNr needs to enable cursorline in set.lua
-	-- vim.api.nvim_set_hl(0, 'LineNr', { fg = "white"})
-	-- vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = "green"})
+	-- Set number line colors
+	-- vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#51B3EC", bold = true })
+	-- vim.api.nvim_set_hl(0, "LineNr", { fg = "#51B3EC", bold = true })
+	-- vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#51b3ec", bold = true })
+	-- CursorLineNr needs to enable cursorline in set.lua
+	-- vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#51b3af" })
 end
 
 ColorTerminal()
