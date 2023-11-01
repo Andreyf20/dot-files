009bb23f38fa98665f809e34b7b00fc21aad316e
Andrey Torres <atorresf20@gmail.com>
2023-10-31 20:25:35 -0600

    Updated floaterm command

--- nvim/after/plugin/floaterm.lua
+++ nvim/after/plugin/floaterm.lua
@@ -1,1 +1,1 @@
-vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>:FloatermNew lazygit<CR>", {})
+vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>:FloatermNew --width=0.99 --height=0.99 lazygit<CR>", {})
