0c9c7c924cabbac1ae9863bf4ebb04dce2ac292b
Andrey Torres <atorresf20@gmail.com>
2024-06-19 15:48:28 -0600

    Updated zsh custom commands

--- zshrc_custom_commands.sh
+++ zshrc_custom_commands.sh
@@ -1,5 +1,6 @@
 # Git dir, change accordingly
-echo '\n' >> ~/.zshrc
+echo 'Setting up zshrc config'
+echo -e '\n' >> ~/.zshrc
 echo '# My Custom Commands' >> ~/.zshrc
 echo 'alias cdg="cd ~/Documents"' >> ~/.zshrc 
 # Refresh git repo and pull, DEPRECATED: not using this anymore since i use Lazygit
@@ -18,31 +19,37 @@ echo 'alias cat="bat"' >> ~/.zshrc # Remember to install bat
 echo 'alias tmux="tmux -u"' >> ~/.zshrc
 echo 'alias lg="lazygit"' >> ~/.zshrc
 # Setup fzf to work with zsh command history ctrl+r
+echo -e '\n' >> ~/.zshrc
 echo 'source <(fzf --zsh)' >> ~/.zshrc
 echo 'HISTFILE=~/.zsh_history' >> ~/.zshrc
 echo 'HISTSIZE=10000' >> ~/.zshrc
 echo 'SAVEHIST=10000' >> ~/.zshrc
 echo 'setopt appendhistory' >> ~/.zshrc
 echo '# My Custom Commands end' >> ~/.zshrc
-echo '\n' >> ~/.zshrc
+echo -e '\n' >> ~/.zshrc
 # Setup go path
 echo 'export GOPATH="$HOME/go"' >> ~/.zshrc
 echo 'export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin' >> ~/.zshrc
 # Setup dotnet path
 echo 'export PATH="$PATH:$HOME/.dotnet/tools"' >> ~/.zshrc
+echo 'Finished setting up zshrc config'
 
 # Tmux conf
+echo 'Setting up tmux config'
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &&
 cp ./tmux.conf ~/.tmux.conf
+echo 'Finished setting up tmux config'
 # Make sure to run prefix + I to install plugins
 
 # Nvim conf
+echo 'Setting up nvim config'
 mkdir ~/.config/ &&
 cp -r ./nvim ~/.config/nvim/ &&
 rm ~/.config/nvim/README.md
+echo 'Finished setting up nvim config'
 
-# Lazygit conf, the linux dir and the macos dir is different (~/Library/Application Support/lazygit)
-# Untested maybe it works?
+# Lazygit conf, the linux dir and the macos dir is different
+echo 'Setting up lazygit config'
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   # Linux
   mkdir ~/.config/lazygit/ &&
@@ -55,3 +62,5 @@ else
   # Unknown.
   echo 'Error: Unknown OS, cannot set lazygit config do it manually'
 fi
+echo 'Finished setting up lazygit config'
+
