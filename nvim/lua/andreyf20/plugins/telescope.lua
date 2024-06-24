be8eb0da5a267052bb319e0e32045e0893de60ef
Andrey Torres <atorresf20@gmail.com>
2024-06-24 08:09:17 -0600

    Updated nvim telescope

--- nvim/lua/andreyf20/plugins/telescope.lua
+++ nvim/lua/andreyf20/plugins/telescope.lua
@@ -1,11 +1,9 @@
 -- Fuzzy finder, make sure to install ripgrep fd
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.4",
+	tag = "0.1.8",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		local builtin = require("telescope.builtin")
-
 		require("telescope").setup({
 			defaults = {
 				ripgrep_arguments = {
@@ -20,10 +18,10 @@ return {
 			},
 		})
 
+		local builtin = require("telescope.builtin")
+
 		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-		vim.keymap.set("n", "<leader>ps", function()
-			builtin.live_grep()
-		end)
+		vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
 	end,
 }
