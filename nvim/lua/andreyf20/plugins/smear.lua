b38aa238a14b9112ea2caa95b2d68e3f854325c5
Andrey Torres <atorresf20@gmail.com>
2025-09-15 11:18:46 -0600

    Added smear.lua to nvim config

---
+++ nvim/lua/andreyf20/plugins/smear.lua
@@ -0,0 +1,9 @@
+-- Remove this when Ghostty gets its own smear-cursor
+-- Kitty already has a smear cursor
+
+-- Doesn't seem to work well at least not when tested
+-- The cursor color is changing when moving around which is annoying
+return {
+	-- "sphamba/smear-cursor.nvim",
+	-- opts = {},
+}
