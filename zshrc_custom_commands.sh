18a30e17549db25fe52c2ed8429fc1bfec237a94
Andrey Torres <atorresf20@gmail.com>
2025-12-03 21:22:36 -0600

    Removed unused zsh commands

--- zshrc_custom_commands.sh
+++ zshrc_custom_commands.sh
@@ -7,18 +7,10 @@ cd "$(dirname "$0")"
 echo 'Setting up zshrc config'
 echo -e '\n' >> ~/.zshrc
 echo '# My Custom Commands' >> ~/.zshrc
-echo 'alias cdg="cd ~/Documents"' >> ~/.zshrc 
-# Refresh git repo and pull, DEPRECATED: not using this anymore since i use Lazygit
-echo 'alias grefresh="git fetch --prune origin && git pull --ff"' >> ~/.zshrc
-# Command to copy nvim settings from local to the repo dir
-echo 'alias cpnvimconfig="cp -r ~/.config/nvim ~/Documents/dot-files/ && cd ~/Documents/dot-files/"' >> ~/.zshrc
 # Open neovim
-echo 'alias n="nvim ./"' >> ~/.zshrc
 echo 'alias nzsh="nvim ~/.zshrc"' >> ~/.zshrc
 echo 'alias nconfig="nvim ~/.config/nvim"' >> ~/.zshrc
 echo 'alias n.config="nvim ~/.config/"' >> ~/.zshrc
-echo 'alias fzff="cd && fzf --preview=\"cat {}\" | xargs -r nvim"' >> ~/.zshrc # xargs prevents nvim from opening an empty file
-echo 'alias fzfd="cd && find . -type d -print | fzf | xargs -r nvim"' >> ~/.zshrc
 # Other extra commands
 echo 'alias cat="bat"' >> ~/.zshrc # Remember to install bat
 echo 'alias tmux="tmux -u"' >> ~/.zshrc
@@ -43,6 +35,7 @@ echo 'export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin' >> ~/.zshrc
 # Setup dotnet path
 echo 'export PATH="$PATH:$HOME/.dotnet/tools"' >> ~/.zshrc
 echo 'export EDITOR=nvim' >> ~/.zshrc
+# Setup zoxide in zsh
 echo 'eval "$(zoxide init zsh)"' >> ~/.zshrc
 echo 'Finished setting up zshrc config'
 
