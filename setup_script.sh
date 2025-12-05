7c5f1479cbb0331994c4b23f1a0d16624e8c3038
Andrey Torres <atorresf20@gmail.com>
2025-12-05 16:45:52 -0600

    Reorganized setup script

--- setup_script.sh
+++ setup_script.sh
@@ -2,10 +2,42 @@
 
 mkdir ~/.config &> /dev/null
 
+# Nvim config
+chmod +x ./nvim/install_nvim.sh
+./nvim/install_nvim.sh
+echo -e '\n'
+
+# tmux config
+# Make sure to run prefix + I to install plugins
+chmod +x ./tmux/install_tmux.sh
+./tmux/install_tmux.sh
+echo -e '\n'
+
+# lazygit config
+chmod +x ./lazygit/install_lazygit.sh
+./lazygit/install_lazygit.sh
+echo -e '\n'
+
+# zed config
+chmod +x ./zed/install_zed.sh
+./zed/install_zed.sh
+echo -e '\n'
+
+# ghostty config
+chmod +x ./ghostty/install_ghostty.sh
+./ghostty/install_ghostty.sh
+echo -e '\n'
+
+# fastfetch config
+chmod +x ./fastfetch/install_fastfetch.sh
+./fastfetch/install_fastfetch.sh
+echo -e '\n'
+
 # zshrc config
 echo 'zsh config? y/N'
 read confirm
 if [[ $confirm == "y" ]]; then
+  chmod +x ./zshrc_custom_commands.sh
   ./zshrc_custom_commands.sh
   echo -e '\n'
 fi
@@ -18,36 +50,6 @@ if [[ $confirm == "y" ]]; then
   ./fish/install_fish.sh
   echo -e '\n'
 fi
-
-# tmux config
-# Make sure to run prefix + I to install plugins
-echo 'tmux config? y/N'
-read confirm
-if [[ $confirm == "y" ]]; then
-  chmod +x ./tmux/install_tmux.sh
-  ./tmux/install_tmux.sh
-fi
-echo -e '\n'
-
-# Nvim config
-chmod +x ./nvim/install_nvim.sh
-./nvim/install_nvim.sh
-echo -e '\n'
-
-# lazygit config
-chmod +x ./lazygit/install_lazygit.sh
-./lazygit/install_lazygit.sh
-echo -e '\n'
-
-# ghostty config
-chmod +x ./ghostty/install_ghostty.sh
-./ghostty/install_ghostty.sh
-echo -e '\n'
-
-# fastfetch config
-chmod +x ./fastfetch/install_fastfetch.sh
-./fastfetch/install_fastfetch.sh
-echo -e '\n'
 
 # MangoHud config
 echo 'MangoHud config? y/N'
@@ -79,8 +81,4 @@ if [[ $confirm == "y" ]]; then
   ./satty/install_satty.sh
 fi
 echo -e '\n'
-
-# zed config
-chmod +x ./zed/install_zed.sh
-./zed/install_zed.sh
 
