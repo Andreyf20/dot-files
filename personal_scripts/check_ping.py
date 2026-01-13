7e07fa7352550c94c1a56e74820dc8f721234aea
Andrey Torres <atorresf20@gmail.com>
2026-01-12 21:33:31 -0600

    Added logging to check_ping script

--- personal_scripts/check_ping.py
+++ personal_scripts/check_ping.py
@@ -1,17 +1,38 @@
 #!/usr/bin/env python3
 
+from typing import List
 from subprocess import run
 from subprocess import PIPE
 from subprocess import DEVNULL
+from datetime import datetime
 from time import sleep
 from sys import argv
+import os
 
 ip_address = argv[1] if len(argv) > 1 else "8.8.8.8"
 max_acceptable_ms = argv[2] if len(argv) > 2 else 80.0
 print(f"Pinging address: {ip_address}")
 print(f"Max acceptable ms: {max_acceptable_ms}")
 
-sleep(2)
+
+def create_log(start_time: datetime,
+               end_time: datetime,
+               values: List[float]) -> None:
+    if len(values) < 2:
+        return
+    logs_dir = os.path.expanduser(
+        "~/.config/personal_scripts/check_ping_logs/")
+    os.makedirs(logs_dir, exist_ok=True)
+    with open(logs_dir+f"log_{start_time}_{end_time}.txt", "w") as log:
+        values_avg = sum(values) // len(values)
+        values_sorted = sorted(values)
+        values_max = values_sorted[-1]
+        values_mean = values_sorted[len(values) // 2]
+        log.write(f"Start Time: {start_time}\n")
+        log.write(f"End Time: {end_time}\n")
+        log.write(f"Average: {values_avg}\n")
+        log.write(f"Max: {values_max}\n")
+        log.write(f"Mean: {values_mean}\n")
 
 
 def notify_desktop(message: str) -> None:
@@ -21,6 +42,10 @@ def notify_desktop(message: str) -> None:
 
 
 try:
+    start_time = None
+    end_time = None
+    previous_borked = False
+    values = []
     while True:
         result = run(["ping",
                       ip_address,
@@ -37,8 +62,18 @@ try:
         sleep_amount = 5
 
         if max_value > max_acceptable_ms:
-            notify_desktop("PING EXCEEDED!!!!")
+            if previous_borked is False:
+                start_time = datetime.now()
+                notify_desktop("PING EXCEEDED!!!!")
+            previous_borked = True
+            values.append(max_value)
             sleep_amount = 15
+        else:
+            if previous_borked:
+                end_time = datetime.now()
+                create_log(start_time, end_time, values)
+                values.clear()
+            previous_borked = False
 
         sleep(sleep_amount)
 
