db76b979c2d48bb33d4109965878638492dde6a2
Andrey Torres <atorresf20@gmail.com>
2026-01-13 11:19:35 -0600

    Formatted oil.lua

--- nvim/lua/andreyf20/plugins/oil.lua
+++ nvim/lua/andreyf20/plugins/oil.lua
@@ -1,12 +1,14 @@
 return {
-    'stevearc/oil.nvim',
-    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    "stevearc/oil.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
     lazy = false,
     config = function()
         require "oil".setup({
             view_options = {
-                show_hidden = true
-            }
+                show_hidden = true,
+            },
         })
         vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
